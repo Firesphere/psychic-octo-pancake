e8e33611420b3da1a6eb58e6a3816da1f37c5235
Simon Erkelens <github@casa-laguna.net>
2024-01-03 08:35:23 +1300

    Add admin for job hunt status

---
+++ jobhunt/src/Admins/HuntAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Firesphere\JobHunt\Admins;
+
+use Firesphere\JobHunt\Models\Company;
+use Firesphere\JobHunt\Models\Status;
+use SilverStripe\Admin\ModelAdmin;
+
+/**
+ * Class \Firesphere\JobHunt\Admins\HuntAdmin
+ *
+ */
+class HuntAdmin extends ModelAdmin
+{
+
+    private static $url_segment = 'jobhuntadmin';
+
+    private static $menu_title = 'Job hunt';
+
+    private static $managed_models = [
+        Status::class,
+    ];
+}
